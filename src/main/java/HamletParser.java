a71655ce1ae7197ca71e4e13b989c6b57d200266
DeeptiMishra@1512 <mishra.deeptimishra@gmail.com>
2024-12-08 16:06:55 -0500

    commit 1 for addind new method and test cases

--- src/main/java/HamletParser.java
+++ src/main/java/HamletParser.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by thook on 10/7/15.
@@ -22,6 +24,7 @@ public class HamletParser {
             while(scanner.hasNextLine()){
                 String line = scanner.nextLine();
                 result.append(line).append("\n");
+
             }
 
             scanner.close();
@@ -32,7 +35,28 @@ public class HamletParser {
         return result.toString();
     }
 
+
     public String getHamletData(){
+        return hamletData;
+
+    }
+
+    public String patternMatcherMethod(){
+        //Create 2 String variable to store the strings to be matched in file data
+        // adding //b to make sure Horatio and Tariq are a single words when being replaced.
+        String text1 = "//bHoratio//b";
+        String text2 = "//bTariq//b";
+
+        //Creating Pattern and Matcher for Hamlet
+        Pattern hamletPattern = Pattern.compile(text1, Pattern.CASE_INSENSITIVE);
+        Matcher hamletMatcher = hamletPattern.matcher(hamletData);
+        hamletData = hamletMatcher.replaceAll("Leon");
+
+        // Replace "Horatio" with "Tariq"
+        Pattern horatioPattern = Pattern.compile(text2, Pattern.CASE_INSENSITIVE);
+        Matcher horatioMatcher = horatioPattern.matcher(hamletData);
+        hamletData = horatioMatcher.replaceAll("Tariq");
+
         return hamletData;
     }
 
--- src/test/java/HamletParserTest.java
+++ src/test/java/HamletParserTest.java
@@ -15,17 +15,46 @@ public class HamletParserTest {
 
     @Test
     public void testChangeHamletToLeon() {
+
+
     }
 
     @Test
     public void testChangeHoratioToTariq() {
+    HamletParser parser = new HamletParser();
+    String originalText = parser.getHamletData();
+    //checking if before replacing the string if file contains the original string Horatio
+    assertTrue(originalText.contains("Horatio"));
+
+        //Calling method to replace Horatio with "Tariq"
+         parser.patternMatcherMethod();
+
+        //Storing the file data in new String variable and
+        //checking if new data has Horatio string , assert should be false
+        //assert to check if new data contains "Tariq" should be true
+    String changedText = parser.getHamletData();
+    System.out.println(changedText);
+    assertFalse(changedText.contains("Horatio"));
+    assertTrue(changedText.contains("Tariq"));
     }
 
     @Test
     public void testFindHoratio() {
+    HamletParser Parser = new HamletParser();
+    String originalText = Parser.getHamletData();
+
+    assertTrue(originalText.contains("Horatio"));
+
     }
 
     @Test
     public void testFindHamlet() {
+
+        HamletParser parser = new HamletParser();
+        String originalText = parser.getHamletData();
+
+        // Check if "Hamlet" is present in the original text
+        assertTrue(originalText.contains("Hamlet"));
+
     }
 }
